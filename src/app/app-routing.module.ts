5d7526db19be0c5fe6d18cd4bbd00abc6b3a3a6d
Thandolwenkosi Magagula <106537445+tsgmagagul@users.noreply.github.com>
2022-09-29 14:58:34 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BodyComponent } from './body/body.component';
+import { OurServiceComponent } from './our-service/our-service.component';
 import { RegisterLoginComponent} from './register-login/register-login.component';
 const routes: Routes = [
   {path:'',component:RegisterLoginComponent},
-  {path:'body',component:BodyComponent}
+  {path:'body',component:BodyComponent},
+  {path:'our_service',component:OurServiceComponent}
 ];
 
 @NgModule({
