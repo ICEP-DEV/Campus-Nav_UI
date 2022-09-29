f05b483a2e9d578f9c73a03d65e81d85934f3841
Thandolwenkosi Magagula <106537445+tsgmagagul@users.noreply.github.com>
2022-09-29 14:56:04 +0200

    Add files via upload

---
+++ src/app/our-service/our-service.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-our-service',
+  templateUrl: './our-service.component.html',
+  styleUrls: ['./our-service.component.scss']
+})
+export class OurServiceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
